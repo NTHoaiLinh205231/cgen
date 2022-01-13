fa5348ea6a1c2d56b72b6373c9520d0fa64893bd
NTHoaiLinh205231 <NTHoaiLinh205231@users.noreply.github.com>
2022-01-13 15:45:54 +0000

    deploy: 7dc3ddcc788a9293bbf605d1d4cb1deda21dc029

---
+++ search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "acfghiv",
+  1: "gh",
+  2: "gh",
+  3: "gh",
+  4: "afiv",
+  5: "gh",
+  6: "gh",
+  7: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "defines",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Tất cả",
+  1: "Cấu trúc dữ liệu",
+  2: "Files",
+  3: "Các hàm",
+  4: "Các biến",
+  5: "Typedefs",
+  6: "Định nghĩa",
+  7: "Trang"
+};
+
